63a378393ba19a727ede63514909e0e7b9dd7b9d
KamiDummy <pin.inci@hotmail.com>
2026-03-29 22:32:06 -0400

    Lab 08 - Angular Data Binding and Directives

---
+++ Lab8_101503300/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RemoveSpacesPipe } from '../pipes/remove-spaces.pipe';
+import { InputFormatDirective } from '../directives/input-format.directive';
+import { HEROES } from './mock-heroes';
+
+export interface Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, RemoveSpacesPipe, InputFormatDirective],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero: Hero | null = null;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
---
+++ Lab8_101503300/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './heroes.component';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Superman' },
+  { id: 2, name: 'Spider-Man' },
+  { id: 3, name: 'Iron-Man' },
+  { id: 4, name: 'She-Hulk' },
+  { id: 5, name: 'Batman' },
+  { id: 6, name: 'Wonder-Woman' },
+  { id: 7, name: 'Captain-America' },
+  { id: 8, name: 'Black-Widow' },
+  { id: 9, name: 'Thor' },
+  { id: 10, name: 'Ant-Man' },
+];
